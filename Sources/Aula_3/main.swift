53205484b1e5eacb90030b80a70dafd5a64b623f
ANTONIO COSTA SATIRO DE SOUZA <10723636@mackenzista.com.br>
2025-09-04 00:27:17 +0000

    feat 'Aula3 finalizada'

--- Sources/Aula_3/main.swift
+++ Sources/Aula_3/main.swift
@@ -95,4 +95,67 @@ print(meuSaldo)
 
 */
 
+/**
+class PerfilUsuario {
 
+    public let id: Int
+    public let dataDeCadastro: String
+    public var nomeDeExibicao: String
+    private var hashDaSenha: String
+    public var senhaTemp: String
+
+    init (nomeDeExibicao: String, hashDaSenha: String, dataDeCadastro: String){
+
+        self.nomeDeExibicao = nomeDeExibicao
+        self.hashDaSenha = hashDaSenha + "_hashed"
+        self.id = Int.random(in: 1000000000..<9999999999)
+        self.dataDeCadastro = dataDeCadastro
+        self.senhaTemp = "_"
+
+    }
+
+    public func alterarSenha(novaSenha:String){
+
+        if(novaSenha.count >= 8){
+
+            hashDaSenha = novaSenha + "_hashed"
+
+        }
+        else{
+
+            print("Caracteres insuficientes")
+        }
+
+    }
+
+    public func autenticar(senha: String) ->Bool{
+
+        senhaTemp = senha + "_hashed"
+
+        if(senhaTemp == hashDaSenha){
+
+            print("Acesso liberado")
+            return true
+        }
+        else{
+
+            print("Senha incorreta")
+            return false
+        }
+
+
+    }
+
+}
+
+
+let usuario = PerfilUsuario(nomeDeExibicao: "Alex", hashDaSenha: "senhaSegura123", dataDeCadastro: "03/09/2025")
+
+print(usuario.id)
+usuario.alterarSenha(novaSenha: "lelelelele")
+usuario.alterarSenha(novaSenha: "rerce")
+
+let m1 = usuario.autenticar(senha: "lelelelele")
+print(m1)
+
+*/
